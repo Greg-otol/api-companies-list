be09042ae8af44ab53a773662dd00e211ede98d3
Gregório Neto <gregoriogrgeotavia@hotmail.com>
2023-07-14 10:31:51 -0300

    chore: table modifications

---
+++ Migrations/20230714121813_CodeForString.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_companies_list.Migrations
+{
+    /// <inheritdoc />
+    public partial class CodeForString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Codigo",
+                table: "Companies",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Codigo",
+                table: "Companies",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
